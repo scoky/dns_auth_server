8c1bf38405df65e29dbe31ee86f14b861b2d0bff
Kyle Schomp <kgs7@case.edu>
2015-04-07 14:09:48 -0400

    added quotes?

--- web_exp_server.py
+++ web_exp_server.py
@@ -105,7 +105,7 @@ get_fdns_db = ("SELECT src_ip, open, preplay "
 add_fdns_db = ("INSERT INTO fdns "
                "(exp_id, src_ip, open, preplay) "
                "VALUES (%s, %s, %s, %s)")
-add_timing_db = ("UPDATE queries SET resolution = %s WHERE exp_id = %s AND query = %s")
+add_timing_db = ("UPDATE queries SET resolution = %s WHERE exp_id = %s AND query = '%s'")
 
 class WebRoot(Controller):
     def timing(self, exp_id=None, ip=None):
